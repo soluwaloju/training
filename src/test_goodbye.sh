d9e356b0572ab5ab05197c11562d7fcd77dd2a33
Seun Oluwaloju <seun.oluwaloju@circleci.com>
2022-10-05 15:26:26 -0500

    add wait time to tests

--- src/test_goodbye.sh
+++ src/test_goodbye.sh
@@ -2,3 +2,9 @@
 #
 # Tests that we can echo Goodbye.
 echo "Goodbye"
+sleep 2
+echo "Goodbye"
+sleep 3
+echo "Goodbye"
+sleep 5
+echo "Goodbye"
--- src/test_hello.sh
+++ src/test_hello.sh
@@ -2,3 +2,13 @@
 #
 # Tests that we can echo Hello.
 echo "Hello"
+sleep 2
+echo "Hello"
+sleep 2
+echo "Hello"
+sleep 2
+echo "Hello"
+sleep 2
+echo "Hello"
+sleep 2
+echo "Hello"
